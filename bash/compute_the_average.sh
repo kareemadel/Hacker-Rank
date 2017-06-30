d2f5cd6cdda880e4eebef3ac962f7ae44c60decf
Kareem Adel <kareem.adel.000@gmail.com>
2017-06-30 09:53:29 +0200

    used for loop with a more elegant and compact sytax

--- bash/compute_the_average.sh
+++ bash/compute_the_average.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-read N
-sum=0
-i=1
-while [ "$i" -le "$N" ]
-do
-	read J
-	((sum += J))
-	((i += 1))
-done
-echo "$sum"
-printf "%0.03f\n" $(bc -l <<< ""$sum"/"$N"")
+#read N
+#sum=0
+#i=1
+#while [ "$i" -le "$N" ]
+#do
+#	read J
+#	((sum += J))
+#	((i += 1))
+#done
+#echo "$sum"
+#printf "%0.03f\n" $(bc -l <<< ""$sum"/"$N"")
 
+read n
+sum=0
+for ((i=0; i < $n; i++))
+do
+    read x
+    sum=$(( $sum+$x ))
+done
+printf "%0.4f\n" $(echo "$sum/$n" | bc -l)
